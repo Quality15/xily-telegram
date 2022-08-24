c1455da07b1c1eab8fda1eeeec99b11106693b44
Quality <qwerty15092008@gmail.com>
2022-08-24 15:32:12 +0300

    Setting up bot

---
+++ bot.py
@@ -0,0 +1,12 @@
+from aiogram import Bot, types, Dispatcher, executor
+
+token = open('token.ini', 'r').read()
+
+bot = Bot(token=token, parse_mode='html')
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def start(message: types.Message):
+    await bot.send_message(message.chat.id, f'Интеграция Дискорд бота в Телеграм')
+
+executor.start_polling(dp, skip_updates=False) # skip_updates = True
